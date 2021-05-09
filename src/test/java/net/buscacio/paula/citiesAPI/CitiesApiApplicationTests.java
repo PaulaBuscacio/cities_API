554b24449528dc3cf2adc44cab2085c45479300e
PaulaBuscacio <paula@buscacio.net>
2021-05-09 20:01:28 -0300

    initialization

---
+++ src/test/java/net/buscacio/paula/citiesAPI/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.buscacio.paula.citiesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
